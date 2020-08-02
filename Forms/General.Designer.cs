b0de9fc7f6c972349f3cee0c8effec200e03eec7
Денис Голубев (Работа) <amigo36@yandex.ru>
2020-08-02 12:28:36 +0300

    Все начинаю заново

--- Forms/General.Designer.cs
+++ Forms/General.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace SuperJet_3._1
+﻿namespace SuperJet_3._1.Forms
 {
-    partial class Form_General
+    partial class General
     {
         /// <summary>
         /// Required designer variable.
@@ -34,45 +34,39 @@
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.White;
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button1.Font = new System.Drawing.Font("Arial Black", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.BackColor = System.Drawing.Color.GreenYellow;
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(765, 192);
+            this.button1.Size = new System.Drawing.Size(776, 200);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Запуск приложения";
+            this.button1.Text = "Начать работать";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.BackColor = System.Drawing.Color.White;
-            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button2.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button2.Font = new System.Drawing.Font("Arial Black", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(12, 210);
+            this.button2.BackColor = System.Drawing.Color.Tomato;
+            this.button2.Font = new System.Drawing.Font("Arial Narrow", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(12, 218);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(765, 192);
+            this.button2.Size = new System.Drawing.Size(776, 200);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Закрыть приложение";
+            this.button2.Text = "Выход из программы";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // Form_General
+            // General
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(789, 413);
+            this.ClientSize = new System.Drawing.Size(800, 430);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "Form_General";
+            this.Name = "General";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form_General";
+            this.Text = "General";
             this.ResumeLayout(false);
 
         }
