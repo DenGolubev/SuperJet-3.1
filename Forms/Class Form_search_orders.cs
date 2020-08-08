bae4f903c7352173b5688c88f71e7d44183110fa
DenGolubev <68464903+DenGolubev@users.noreply.github.com>
2020-08-08 22:32:43 +0300

    Добавил две формы, возврат нарядов и поиск нарядов. Работаю с формами

---
+++ Forms/Class Form_search_orders.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperJet_3._1
+{
+    class Class_Form_search_orders: Class_BaseForm
+    {
+    }
+}
